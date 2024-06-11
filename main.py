a99191b9fce9938ee1b46091f4cc3c4e6315c8bb
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2024-06-10 22:16:58 -0500

    Fix en Service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException, Body
 from fastapi.responses import HTMLResponse
-from typing import List
+from typing import List, Optional
+from pydantic import BaseModel, Field
 
 app=FastAPI(
     title="FastApi CRUD",
@@ -8,17 +9,16 @@ app=FastAPI(
     version="0.0.1",
 )
 
-movies = [
-    {
-        'id': 1,
-        'title': 'El padrino',
-        'overview': 'El padrino es una pelicula de 1972 dirigida por Francis Ford Coppola',
-        'year': 1972,
-        'rating': 8.7,
-        'category': 'Action'
-    }
-]
-
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview:str
+    year: int
+    rating: float
+    category: str
+    
+movies=[]
+    
 @app.get("/api", tags=['Start Root'])
 def readRoot():
     return HTMLResponse("<h1>FastApi</h1>")
@@ -33,8 +33,8 @@ def getMovieById(id: int):
         raise HTTPException(status_code=400, detail='Id no valido')
     try:
         for movie in movies:
-            if movie['id'] == id:
-                return movie       
+            if movie.id == id:
+                return movie
         else:  
             raise HTTPException(status_code=404, detail=f'Pelicula con el id: {id} no encontrada')
     except Exception as e: 
@@ -46,7 +46,7 @@ async def getMoviesByCategory(category: str):
     if category is None or category == '':
         raise HTTPException(status_code=400, detail="Categoria no valida")
     try:
-        foundMovies = [movie for movie in movies if movie['category'].lower() == category.lower()]
+        foundMovies = [movie for movie in movies if movie.category.lower() == category.lower()]
         if not foundMovies:
             raise HTTPException(status_code=404, detail=f"No hay peliculas encontradas con la categoria:'{category}'.")
         return foundMovies
@@ -55,66 +55,35 @@ async def getMoviesByCategory(category: str):
 
 
 @app.post('/movies', tags=['Create A Movie'])
-def createMovie(
-    id: int = Body(),
-    title: str = Body(),
-    overview: str = Body(),
-    year: int = Body(),
-    rating: float = Body(),
-    category: str = Body()
-    ):
+def createMovie(movie: Movie):
     try:
-        if any(movie['id'] == id for movie in movies):
+        if any(item['id'] == id for item in movies):
             raise HTTPException(status_code=400, detail=f"Pelicula con el id: {id} ya existe")
         
-        movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-        })
+        movies.append(movie)
         return "Pelicula creada correctamente"
+    
     except Exception as e: 
         raise HTTPException(status_code=500, detail="Ha ocurrido un error al procesar tu solicitud")
 
-
-@app.put('/movies/update/{id}', tags=['Update Movie By Id'])
-def updateMovieById(
-    id: int,
-    title: str = Body(),
-    overview: str = Body(),
-    year: int = Body(),
-    rating: float = Body(),
-    category: str = Body()
-                    ):
-    if id is None or id <= 0: 
-        raise HTTPException(status_code=400, detail="Id no valido")
-    try:
-        for movie in movies:
-            if movie['id'] == id:
-                movie['title'] = title
-                movie['overview'] = overview
-                movie['year'] = year
-                movie['rating'] = rating
-                movie['category'] = category
-                return "Pelicula actualizada correctamente"
-        else: 
-            raise HTTPException(status_code=404, detail=f"Pelicula con el id: {id} no encontrada")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Ha ocurrido un error al procesar tu solicitud")
     
-
+@app.put("/movies/update/{id}", tags=["Update Movie By Id"])
+def updateMovieById(id: int, updatedMovie: Movie):
+    for i, movie in enumerate(movies):
+        if movie.id == id:
+            movies[i] = updatedMovie
+            return {"message": "Pelicula actualizada correctamente"}
+    raise HTTPException(status_code=404, detail=f"Pelicula con el id: {id} no encontrada")
+                        
 @app.delete('/movies/delete/{id}', tags=['Delete Movie By Id'])
 def deleteMovieById(id: int):
     if id is None or id <= 0: 
         raise HTTPException(status_code=400, detail="Id no valido")
     try:
-        for movie in movies:
-            if movie['id'] == id:
-                movies.remove(movie)
-                return "Pelicula eliminada correctamente"
+        for i, movie in enumerate(movies):
+            if movie.id == id:
+                del movies[i]
+                return {"message": "Pelicula eliminada correctamente"}
         else: 
             raise HTTPException(status_code=404, detail=f"Pelicula con el id: {id} no encontrada")
     except Exception as e:
